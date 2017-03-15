8c86fdf053010a10bffcb1c0717568929070f5f1
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-03-15 13:37:23 -0600

    Add exception allowing linking with ARB

--- src/cseq_impl.h
+++ src/cseq_impl.h
@@ -2,19 +2,29 @@
 Copyright (c) 2006-2017 Elmar Pruesse <elmar.pruesse@ucdenver.edu>
 
 This file is part of SINA.
+123456789012345678901234567890123456789012345678901234567890123456789012
+SINA is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
 
-SINA is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+SINA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with SINA.  If not, see <http://www.gnu.org/licenses/>.
+
+Additional permission under GNU GPL version 3 section 7
+
+If you modify SINA, or any covered work, by linking or combining it
+with components of ARB (or a modified version of that software),
+containing parts covered by the terms of the
+ARB-public-library-license, the licensors of SINA grant you additional
+permission to convey the resulting work. Corresponding Source for a
+non-source form of such a combination shall include the source code
+for the parts of ARB used as well as that of the covered work.
 */
 
 #ifndef _CSEQ_IMPL_H_
