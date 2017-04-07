6d04997099471b2d007b90a19a38e57baef829f7
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-04-07 17:58:22 -0600

    Don't drive tests with xml output
    Doesn't seem to work?!

--- tools/test_driver.sh
+++ tools/test_driver.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # get last argument
-env
+#env
 
 log_base="${@:(-1):1}"
 export LD_LIBRARY_PATH=$ARBHOME/lib
 #exec "$@" --log_level=all --output_format=xml --report_level=detailed \
 #          --report_sink="${log_base}.report.xml" \
 #          --log_sink="${log_base}.log.xml"
-exec "$@" --log_level=all --report_level=no --output_format=xml > ${log_base}.log.xml
+#exec "$@" --log_level=all --report_level=no --output_format=xml > ${log_base}.log.xml
+exec "$@" --log_level=all --report_level=detailed > ${log_base}.log
+
 
