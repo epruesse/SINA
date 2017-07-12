03b4ab130ee57d693949366e43c76ef3468710c5
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-07-11 21:05:48 -0600

    Switch to new caching circleCI setup

--- ci_scripts/install_conda.sh
+++ ci_scripts/install_conda.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
 
-case `uname` in
-    Linux)  CONDA_OSNAME=Linux;;
-    Darwin) CONDA_OSNAME=MacOSX;;
-esac
+CONDA_PACKAGES="autoconf automake libtool toolchain pkg-config boost gcc arb-bio"
+CONDA_PACKAGES="$CONDA_PACKAGES pcre libiconv"
 
-CONDA_BASEURL=https://repo.continuum.io/miniconda
-wget $CONDA_BASEURL/Miniconda3-latest-$CONDA_OSNAME-x86_64.sh -O miniconda.sh
+# Make sure we have conda in the PATH always
+if ! grep $MINICONDA/bin $BASH_ENVx; then
+   echo "Prepending $MINICONDA/bin to PATH in $BASH_ENV"
+   echo export PATH="$MINICONDA/bin:$PATH" >> $BASH_ENV
+   source $BASH_ENV
+fi
 
-bash miniconda.sh -b -p $HOME/miniconda
+# Homemade restore_cache
+if test -e "LOCAL"; then
+    echo "Running locally"
+    if test -e conda.tgz; then
+	echo -n "Unpacking local conda cache... "
+	tar -C / -xzf conda.tgz
+	echo "done"
+    fi
+fi
 
-export PATH="$HOME/miniconda/bin:$PATH"
-export PREFIX="$HOME/miniconda"
-hash -r
+# Install Miniconda if missing
+if test -d $MINICONDA; then
+    echo "Found conda install"
+else
+    case `uname` in
+	Linux)  CONDA_OSNAME=Linux;;
+	Darwin) CONDA_OSNAME=MacOSX;;
+    esac
 
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-for channel in r defaults conda-forge bioconda; do
-    conda config --system --add channels $channel
-done
+    CONDA_BASEURL=https://repo.continuum.io/miniconda
+    curl $CONDA_BASEURL/Miniconda3-latest-$CONDA_OSNAME-x86_64.sh -o miniconda.sh
 
-conda create -n build arb-bio boost pkg-config autoconf automake libtool
-source activate build
+    bash miniconda.sh -b -p $MINICONDA
+    hash -r
 
-export CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib -Wl,-rpath -Wl,$CONDA_PREFIX/lib"
-export PKG_CONFIG_PATH="$CONDA_PREFIX/lib/pkgconfig:$CONDA_PREFIX/share/pkgconfig"
+    conda config --system --set always_yes yes --set changeps1 no
+    conda update -q conda
+    for channel in r defaults conda-forge bioconda; do
+	conda config --system --add channels $channel
+    done
+    conda update -q conda
+fi
+
+# Install/update package
+# (Hope that "install" suffices)
+conda install -q conda $CONDA_PACKAGES
+conda update -q conda $CONDA_PACKAGES
+conda info
+
+# Homemade save_cache
+# Clean out tarballs, update cache if there were any (=> something was installed)
+if conda clean --yes --tarballs | grep Removed; then
+   if test -e "LOCAL"; then
+       echo -n "Packing local conda cache..."
+       tar -C / -czf conda.tgz $MINICONDA
+       echo "done"
+   else
+       (conda info; conda list) >> conda_state.txt
+   fi
+fi
+
+
+
