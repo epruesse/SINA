891fd5d01e08be41618ddbcf4d9aed16ace42942
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-07-11 23:51:46 -0600

    Fix junit path

--- tools/test_driver.sh
+++ tools/test_driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # get last argument
 log_base="${@:(-1):1}"
-
+log_base_noslash=test$(echo $log_base | tr / _)
 exec "$@" \
-     --logger=JUNIT,all,"${JUNIT_REPORT_PATH}${log_base}.xml" \
+     --logger=JUNIT,all,"${JUNIT_REPORT_PATH}${log_base_noslash}.xml" \
      --report_level=detailed > ${log_base}.log
 
 
