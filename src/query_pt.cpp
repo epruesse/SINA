epruesse/SINA

commit 5238d4442677efa6228563a98f48ba6428f7337b

Cleanup query_pt a little

src/query_pt.cpp
@@ -59,14 +59,39 @@ using std::vector;
 #include <boost/algorithm/string/predicate.hpp>
 #include "../include/pstream.h"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+
 boost::mutex arb_pt_start;
 
-using namespace sina;
+namespace sina {
+
+struct query_pt::options {
+};
+struct query_pt::options *query_pt::opts;
+
+po::options_description
+query_pt::get_options_description() {
+    po::options_description od("ARB PT-server search");
+    opts =  new struct query_pt::options();
+    od.add_options()
+        ;
+}
+
+void query_pt::validate_vm(po::variables_map& vm) {
+}
+
 
 struct query_pt::priv_data {
     priv_data(const char* port, const char* db)
-        : portname(port), dbname(db), arb_pt_server(NULL), 
-          range_begin(-1), range_end(-1) {}
+        : portname(port),
+          dbname(db),
+          arb_pt_server(NULL),
+          range_begin(-1),
+          range_end(-1),
+          find_type_fast(false)
+    {}
     aisc_com *link;
     T_PT_MAIN com;
     T_PT_LOCS locs;
@@ -77,7 +102,8 @@ struct query_pt::priv_data {
     redi::pstream *arb_pt_server;
     int range_begin;
     int range_end;
-    
+    bool find_type_fast;
+
     void create_context();
 };
 
@@ -224,10 +250,9 @@ query_pt::restart() {
 }
 
 query_pt::query_pt(const char* portname, const char* dbname)
-    : data(*(new priv_data(portname,dbname))), find_type_fast(false)
+    : data(*(new priv_data(portname,dbname)))
 {
     init();
-    set_find_type_fast(true);
 }
 
 query_pt::~query_pt() {
@@ -242,7 +267,7 @@ query_pt::set_find_type_fast(bool fast) {
                        FAMILYFINDER_FIND_TYPE, fast?1:0,
                        NULL);
     if (err) cerr << "Unable to set find_type" << endl;
-    else find_type_fast = fast;
+    else data.find_type_fast = fast;
 }
 
 void
@@ -358,7 +383,7 @@ match_retry:
     }
 
     err = aisc_get(data.link,
-                       PT_FAMILYFINDER, data.ffinder,
+                   PT_FAMILYFINDER, data.ffinder,
                    FAMILYFINDER_FAMILY_LIST, f_list.as_result_param(),
                    NULL);
     if (err || !f_list.exists() ) {
@@ -534,6 +559,7 @@ query_pt::exception::what() const throw() {
     return message.c_str();
 }
 
+} // namespace sina
 
 #else // TEST
 

src/query_pt.h
@@ -34,57 +34,98 @@ for the parts of ARB used as well as that of the covered work.
 #include <vector>
 #include <exception>
 
+#include <boost/program_options.hpp>
+#include <boost/core/noncopyable.hpp>
+
 namespace sina {
 
 class cseq;
 class query_arb;
 
-class query_pt {
-  query_pt& operator=(const query_pt&);
-  query_pt(const query_pt&);
+class query_pt : private boost::noncopyable {
+private:
+    void init();
+    void exit();
+    void restart();
+public:
+    query_pt(const char* portname, const char* dbname);
+    ~query_pt();
 
-  void init();
-  void exit();
-  void restart();
- public:
-  query_pt(const char* portname, const char* dbname);
-  ~query_pt();
+    /**
+     * match runs a word search using the PT server
+     *
+     * arguments:
+     *  family:    will contain scored results
+     *  query:     query sequence
+     *  min_match: minimum number of results required
+     *  max_match: maximum number of results desired
+     *  min_score: minimum relative score
+     *  max_score: maximum relative score
+     *  arb:       pointer to matching arb database
+     *  noid:      skip matches containing query
+     *  min_len:   skip matches shorter
+     *  num_full:  minimum "full length", ignoring score
+     *  minlen_full: length to be considered "full"
+     *  range_cover: minimum sequences touching alignment edge
+     *  leave_query_out: drop sequence with matching id
+     */
+    double match(std::vector<cseq> &family,
+                 const cseq& query,
+                 int min_match,
+                 int max_match,
+                 float min_score,
+                 float max_score,
+                 query_arb *arb,
+                 bool noid,
+                 int minlen,
+                 int num_full,
+                 int minlen_full,
+                 int range_cover,
+                 bool leave_query_out);
 
-  double match(std::vector<cseq> &family, const cseq& query,
-	       int min_match, int max_match, float min_score, float max_score,
-	       query_arb *arb, bool noid, int minlen,
-	       int num_full, int minlen_full, int range_cover, bool leave_query_out);
+    double match(std::vector<cseq> &family, const cseq& sequence,
+                 int min_match, int max_match, float min_score) {
+        return match(family, sequence, min_match, max_match, min_score, 2.0,
+                     0, false, 0, 0, 0, 0, false);
+    };
 
-  double match(std::vector<cseq> &family, const cseq& sequence,
-	       int min_match, int max_match, float min_score) {
-    return match(family, sequence, min_match, max_match, min_score, 2.0,
-		 0, false, 0, 0, 0, 0, false);
-  };
+    int turn_check(const cseq& query, bool all);
 
-  int turn_check(const cseq& query, bool all);
+    void set_find_type_fast(bool fast);
+    void set_probe_len(int len);
+    void set_mismatches(int len);
+    void set_sort_type(bool absolute);
+    void set_range(int startpos, int stoppos);
+    void unset_range();
 
-  void set_find_type_fast(bool fast);
-  void set_probe_len(int len);
-  void set_mismatches(int len);
-  void set_sort_type(bool absolute);
-  void set_range(int startpos, int stoppos);
-  void unset_range();
+    class exception : public std::exception {
+        std::string message;
+    public:
+        exception(std::string _message) throw();
+        ~exception() throw();
+        virtual const char* what() const throw();
+    };
 
-  class exception : public std::exception {
-      std::string message;
-  public: 
-      exception(std::string _message) throw();
-      ~exception() throw();
-      virtual const char* what() const throw();
-  };
-
- private:
-  struct priv_data;
-  priv_data &data;
-  bool find_type_fast;
-
+    static boost::program_options::options_description get_options_description();
+    static void validate_vm(boost::program_options::variables_map&);
+private:
+    struct priv_data;
+    priv_data &data;
+    struct options;
+    static struct options *opts;
 };
 
 } // namespace sina
 
 #endif // _QUERY_PT_H
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . 0))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :

commit 16507c96742070be54fefc52ea9f520b3093500b

Fix bug introduced in 5238d44

src/query_pt.cpp
@@ -411,7 +411,7 @@ match_retry:
             continue;
         }
 
-        if (find_type_fast) {
+        if (data.find_type_fast) {
             f_relscore *= 4;
         }
 
@@ -475,7 +475,7 @@ match_retry:
                 cerr << "Unable to get next item in family list" << endl;
                 break;
             }
-            if (find_type_fast) {
+            if (data.find_type_fast) {
                 f_relscore *= 4;
             }
 