a44dc14e8c324738e6e826d3afb71d18326e5751
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-08-06 20:28:04 -0600

    More friendly timing output

--- src/timer.h
+++ src/timer.h
@@ -1,33 +1,31 @@
 /*
-Copyright (c) 2006-2017 Elmar Pruesse <elmar.pruesse@ucdenver.edu>
+  Copyright (c) 2006-2017 Elmar Pruesse <elmar.pruesse@ucdenver.edu>
 
-This file is part of SINA.
-123456789012345678901234567890123456789012345678901234567890123456789012
-SINA is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your
-option) any later version.
+  This file is part of SINA.
+  SINA is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your
+  option) any later version.
 
-SINA is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+  SINA is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for more details.
 
-You should have received a copy of the GNU General Public License
-along with SINA.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with SINA.  If not, see <http://www.gnu.org/licenses/>.
 
-Additional permission under GNU GPL version 3 section 7
+  Additional permission under GNU GPL version 3 section 7
 
-If you modify SINA, or any covered work, by linking or combining it
-with components of ARB (or a modified version of that software),
-containing parts covered by the terms of the
-ARB-public-library-license, the licensors of SINA grant you additional
-permission to convey the resulting work. Corresponding Source for a
-non-source form of such a combination shall include the source code
-for the parts of ARB used as well as that of the covered work.
+  If you modify SINA, or any covered work, by linking or combining it
+  with components of ARB (or a modified version of that software),
+  containing parts covered by the terms of the
+  ARB-public-library-license, the licensors of SINA grant you additional
+  permission to convey the resulting work. Corresponding Source for a
+  non-source form of such a combination shall include the source code
+  for the parts of ARB used as well as that of the covered work.
 */
 
-#include <iostream>
 #include <sys/time.h>
 #include <time.h>
 #include <vector>
@@ -36,6 +34,8 @@ for the parts of ARB used as well as that of the covered work.
 #include <iterator>
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <numeric>
 
 #ifndef _TIMER_H_
 #define _TIMER_H_
@@ -51,8 +51,8 @@ namespace sina {
 
 struct timestamp : private timeval {
     timestamp(int) {
-       tv_sec = 0;
-       tv_usec = 0;
+        tv_sec = 0;
+        tv_usec = 0;
     }
 
     void get() {
@@ -99,7 +99,9 @@ struct timestamp : private timeval {
     }
 
     friend std::ostream& operator<<(std::ostream& out, const timestamp& t) {
-        return out << t.tv_sec << "." << std::setfill('0') << std::setw(6) << t.tv_usec;
+        return out << t.tv_sec
+                   << "." << std::setfill('0') << std::setw(3) << t.tv_usec/1000
+                   << " s";
     }
 };
 
@@ -110,13 +112,14 @@ class timer {
     std::vector<const char*> names;
     std::vector<timestamp>::iterator time_it;
     timestamp t_last;
-
+    unsigned int calls;
 public:
-    timer() : timestamps(1,0), t_last(0) {}
+    timer() : timestamps(1,0), t_last(0), calls(0) {}
 
     void start() {
         time_it = timestamps.begin();
         t_last.get();
+        ++calls;
     }
 
     void stop(const char* name=NULL) {
@@ -134,17 +137,20 @@ public:
         time_it-=i;
         t_last.get();
     }
+
     friend std::ostream& operator<<(std::ostream& out , const timer& t) {
-        out << "timed " << t.timestamps.size() << ":";
+        out << std::accumulate(t.timestamps.begin(), t.timestamps.end(), timestamp(0))
+            << " (" << t.calls << " calls, ";
         std::transform(++t.timestamps.begin(), t.timestamps.end(),
                        t.names.begin(),
-                       std::ostream_iterator<std::string>(out, " "),
+                       std::ostream_iterator<std::string>(out, ", "),
                        [](const timestamp &t, const char* name) {
                            std::stringstream tmp;
-                           if (name) tmp << name << "=";
+                           if (name) tmp << name << " = ";
                            tmp << t;
                            return tmp.str();
                        });
+        out << ")";
         return out;
     }
 };
