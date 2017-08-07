0a81e3f963fb7d677d3c9c449edb4a47305e264a
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-08-06 20:28:55 -0600

    Add "ostream << obj" helper calling "obj.print_to" by template

--- src/helpers.h
+++ src/helpers.h
@@ -1,2 +1,14 @@
+#ifndef _HELPERS_H_
+#define _HELPERS_H_
+
 #define unlikely(x) (__builtin_expect ((x), 0))
 #define likely(x) (__builtin_expect ((x), 1))
+
+template<class T>
+auto operator<<(std::ostream& out, const T& t) -> decltype(t.print_to(out)) {
+  // auto f()->decltype() used so that template only applies to
+  // things with proper print_to method
+  return t.print_to(out);
+}
+
+#endif // _HELPERS_H_
