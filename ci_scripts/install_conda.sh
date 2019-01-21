e20209cbaea671651f0ba75d9f5a45eb420bbc5a
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-01-21 12:54:04 -0700

    CI: load conda.sh instead of setting PATH and activate base env

--- ci_scripts/install_conda.sh
+++ ci_scripts/install_conda.sh
@@ -7,10 +7,9 @@
 # - updates packages with every run
 # - needs BASH_ENV to point to the bashrc
 # - needs MINICONDA to point to the miniconda install path
-set -x
 
-CONDA_PACKAGES="autoconf automake libtool toolchain pkg-config boost arb-bio-devel"
-CONDA_PACKAGES="$CONDA_PACKAGES git tbb tbb-devel glib libiconv bc sed sphinx"
+CONDA_PACKAGES="autoconf automake libtool pkg-config boost arb-bio-devel"
+CONDA_PACKAGES="$CONDA_PACKAGES git tbb tbb-devel glib libiconv bc sed sphinx nomkl"
 
 CONDA_BASEURL=https://repo.continuum.io/miniconda
 
@@ -29,27 +28,36 @@ case "$(uname)" in
 	;;
 esac
 
-# Setup PATH
-if test -n "$BASH_ENV"; then
-    echo "Prepending $MINICONDA/bin to PATH in $BASH_ENV"
-    echo export PATH="$MINICONDA/bin:$PATH" >> $BASH_ENV
-fi
-export PATH="$MINICONDA/bin:$PATH" >> $BASH_ENV
-
 # Install Miniconda if missing
 if test -d $MINICONDA; then
     echo "Found conda install"
 else
     curl $CONDA_BASEURL/Miniconda3-latest-$CONDA_OSNAME-x86_64.sh -o miniconda.sh
     bash miniconda.sh -b -p $MINICONDA
-    hash -r
+    source $MINICONDA/etc/profile.d/conda.sh
 
-    conda config --system --set always_yes yes --set changeps1 no
-    conda config --system --add channels defaults
-    conda config --system --add channels bioconda
-    conda config --system --add channels conda-forge
-    conda update -q conda
+    $MINICONDA/bin/conda config --system --set always_yes yes --set changeps1 no
+    $MINICONDA/bin/conda config --system --add channels defaults
+    $MINICONDA/bin/conda config --system --add channels bioconda
+    $MINICONDA/bin/conda config --system --add channels conda-forge
+    $MINICONDA/bin/conda update -q conda
 fi
+
+# Setup Conda
+if test -z "$BASH_ENV"; then
+    BASH_ENV="/tmp/bash_env"
+fi
+cat >>$BASH_ENV <<EOF
+    export MINICONDA=$MINICONDA
+    source \$MINICONDA/etc/profile.d/conda.sh
+    conda activate base
+    export CPPFLAGS="\$CPPFLAGS -I\$CONDA_PREFIX/include"
+    export LDFLAGS="\$LDFLAGS -L\$CONDA_PREFIX/lib"
+    export CXXFLAGS="\$CXXFLAGS -std=c++14"
+EOF
+
+cat $BASH_ENV
+source $BASH_ENV
 
 # Install/update package
 conda install -q conda $CONDA_PACKAGES
